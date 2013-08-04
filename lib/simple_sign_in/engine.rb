e26cd1428e59acb1683a9c9e13dc9605a58d01f8
Izhar Gavish <izharg@gmail.com>
2013-08-04 18:51:46 +0300

    removed error when environment variables are missing

--- lib/simple_sign_in/engine.rb
+++ lib/simple_sign_in/engine.rb
@@ -9,9 +9,11 @@ module SimpleSignIn
     initializer 'simple_sign_in.initialize' do
       config.to_prepare do
         unless ENV['SIMPLE_SIGN_IN_LOGIN'] && ENV['SIMPLE_SIGN_IN_PASSWORD']
-          raise "simple_sign_in could not be loaded because login and password were not specified!
+          puts <<-eos
+            simple_sign_in login and password were not specified!
             please make sure that the environment variables SIMPLE_SIGN_IN_LOGIN and SIMPLE_SIGN_IN_PASSWORD
-            have been set up as described here: https://github.com/Yud/simple_sign_in#installation"
+            have been set up as described here: https://github.com/Yud/simple_sign_in#installation
+          eos
         end
         ActiveSupport.on_load(:action_controller) do
           include SimpleSignIn::ControllerMethods
