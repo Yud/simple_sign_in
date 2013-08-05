e431023cb7cb325d167dd1a38eef8396588d3de1
Izhar Gavish <izharg@gmail.com>
2013-08-05 15:38:45 +0300

    login controller raises error if environment vars are missing and redirects user to root if he's already logged in

--- lib/simple_sign_in/engine.rb
+++ lib/simple_sign_in/engine.rb
@@ -8,13 +8,6 @@ module SimpleSignIn
 
     initializer 'simple_sign_in.initialize' do
       config.to_prepare do
-        unless ENV['SIMPLE_SIGN_IN_LOGIN'] && ENV['SIMPLE_SIGN_IN_PASSWORD']
-          puts <<-eos
-            simple_sign_in login and password were not specified!
-            please make sure that the environment variables SIMPLE_SIGN_IN_LOGIN and SIMPLE_SIGN_IN_PASSWORD
-            have been set up as described here: https://github.com/Yud/simple_sign_in#installation
-          eos
-        end
         ActiveSupport.on_load(:action_controller) do
           include SimpleSignIn::ControllerMethods
         end
--- spec/controllers/simple_sign_in/login_controller_spec.rb
+++ spec/controllers/simple_sign_in/login_controller_spec.rb
@@ -12,6 +12,12 @@ module SimpleSignIn
         get 'new', use_route: SimpleSignIn
         response.should be_success
       end
+
+      it "should redirect to root if the user is already logged in" do
+        session['simple_sign_in_admin'] = @login
+        get 'new', use_route: SimpleSignIn
+        response.should redirect_to "/"
+      end
     end
 
     describe "POST 'create'" do
@@ -28,6 +34,12 @@ module SimpleSignIn
         session[:simple_sign_in_admin].should be_nil
         flash[:notice].should == "Incorrect login or password"
       end
+
+      it "should redirect to root if the user is already logged in" do
+        session['simple_sign_in_admin'] = @login
+        post 'create', login: @login, password: @pass, use_route: SimpleSignIn
+        response.should redirect_to "/"
+      end
     end
 
     describe "DELETE 'destroy'" do
@@ -38,5 +50,12 @@ module SimpleSignIn
         response.should redirect_to :root
       end
     end
+
+    it "should raise an error if environment variables are nil" do
+      ENV['SIMPLE_SIGN_IN_LOGIN'] = nil
+      expect { get 'new', use_route: SimpleSignIn }.to raise_error
+      expect { post 'create', login: '', password: '', use_route: SimpleSignIn }.to raise_error
+      ENV['SIMPLE_SIGN_IN_LOGIN'] = @login
+    end 
   end
 end
